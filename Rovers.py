564ce529f7c0545f9c0381269a694fe1304ac4b9
Martinvivas <129123814+Martinvivas@users.noreply.github.com>
2026-08-24 20:19:24 -0300

    GREEN: Avanza una celda

--- Rovers.py
+++ Rovers.py
@@ -8,6 +8,16 @@ class Rover:
         self.y = y
         self.orientacion = orientacion
 
+    def ejecutar_comando(self, comando):
+        if comando == "F":
+            self.avanza_una_celda()
+        elif comando == "R":
+            self.girar_derecha()
+        elif comando == "L":
+            self.girar_izquierda()
+        elif comando == "B":
+            self.retroceder_una_celda()
+
 
     def informa_posicion(self):
         return (self.x, self.y, self.orientacion)
@@ -15,7 +25,7 @@ class Rover:
     def girar_derecha(self):
      
         if self.orientacion == "Norte":
-         self.orientacion = "Este"
+            self.orientacion = "Este"
         elif self.orientacion == "Este":
             self.orientacion = "Sur"
         elif self.orientacion == "Sur":
@@ -26,14 +36,25 @@ class Rover:
 
     def girar_izquierda(self): 
         if self.orientacion == "Norte":
-          self.orientacion = "Oeste"
+            self.orientacion = "Oeste"
         elif self.orientacion == "Oeste":
-         self.orientacion = "Sur"
+            self.orientacion = "Sur"
         elif self.orientacion == "Sur":
-         self.orientacion = "Este"
+             self.orientacion = "Este"
         elif self.orientacion == "Este":
-         self.orientacion = "Norte"
+            self.orientacion = "Norte"
         return (self.x, self.y, self.orientacion)
 
-    
+    def avanza_una_celda(self):
+        if self.orientacion == "Norte":
+            self.y += 1
+        elif self.orientacion == "Este":
+            self.x += 1
+        elif self.orientacion == "Sur":
+            self.y -= 1
+        elif self.orientacion == "Oeste":
+            self.x -= 1
+        return (self.x, self.y, self.orientacion)
+
+
    
